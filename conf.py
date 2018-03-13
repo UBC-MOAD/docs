4f48f48563aa5862de629406ccc370dd0013bba5
Doug Latornell <djl@douglatornell.ca>
2018-03-13 14:44:29 -0700

    Start writing XIOS-2 docs.

--- conf.py
+++ conf.py
@@ -29,7 +29,15 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.intersphinx',
 ]
+
+intersphinx_mapping = {
+    'nemocmd':
+        ('http://nemo-cmd.readthedocs.io/en/latest/', None),
+    'salishseacmd':
+        ('http://salishseacmd.readthedocs.io/en/latest/', None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
